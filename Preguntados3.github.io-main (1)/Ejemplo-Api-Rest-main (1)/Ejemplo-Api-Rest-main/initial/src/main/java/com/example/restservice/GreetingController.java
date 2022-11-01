bc476c6f200e1686d1f838f724b7a3704f48541a
alangiercowski <81708985+alangiercowski@users.noreply.github.com>
2022-11-01 16:20:35 -0300

    Add files via upload

--- Preguntados3.github.io-main (1)/Ejemplo-Api-Rest-main (1)/Ejemplo-Api-Rest-main/initial/src/main/java/com/example/restservice/GreetingController.java
+++ Preguntados3.github.io-main (1)/Ejemplo-Api-Rest-main (1)/Ejemplo-Api-Rest-main/initial/src/main/java/com/example/restservice/GreetingController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -40,6 +41,16 @@ public class GreetingController {
     @GetMapping("/api/borrarUsuario/{mail}")
     public void borrarUsuario(@PathVariable String mail){
         this.accesoABaseDeDatos.borrarUsuario(mail);
+    }
+    @GetMapping("/api/verHighScore")
+    public void verHighScore(){
+        this.accesoABaseDeDatos.obtenerHighScore();
+    }
+    @CrossOrigin(origins = "http://127.0.0.1:5500")
+    @GetMapping("/api/obtenerPreguntas")
+    public ResponseEntity<Object> obtenerPreguntas(){
+        ArrayList<HashMap<String, String>>preguntas = this.accesoABaseDeDatos.obtenerPreguntas();
+        return new ResponseEntity<>(preguntas, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/datosUsuario/{nombreUsuario}/{clave}",method = RequestMethod.GET)
--- Preguntados3.github.io-main (1)/Preguntados3.github.io-main/script.js
+++ Preguntados3.github.io-main (1)/Preguntados3.github.io-main/script.js
@@ -1,22 +1,34 @@
-document.getElementById("elform").addEventListener("submit", function(event){
 
+let usuarioActual
+
+document.getElementById("elform").onsubmit(function(event){
   event.preventDefault();
-
 });
 function registrar(){
-
-event.preventDefault();
-
-  fetch("http://localhost:8080/api/registrarUsuario/"+ document.getElementById("email").value + "/" + document.getElementById("password").value + "/" + document.getElementById("nombre").value + "/" + document.getElementById("apellido").value).then(function(response){
-    console.log("data");
-    response.json().then(function(data) {
-      
-      if(!data.respuesta){
-        alert("Cuenta ya registrada");
-      }
-        
-    });
+  
+  event.preventDefault();
+    console.log("antes edl fetch");
+    fetch("http://localhost:8080/api/registrarUsuario/"+ document.getElementById("email").value + "/" + document.getElementById("password").value + "/" + document.getElementById("nombre").value + "/" + document.getElementById("apellido").value).then(function(response){
+      response.json().then(function(data) {
+        if(!data.respuesta){
+          alert("Cuenta ya registrada");
+        }else{
+          $("#staticBackdrop2").modal("hide");
+        }
+          
+      });
+  
   });
+
+
+
+}
+function modoObscuro(){
+  document.getElementById("container").style.backgroundColor = "rgb(46, 46, 46)";
+  document.getElementById("btn-1").style.backgroundColor = "rgb(46, 46, 46)";
+  document.getElementById("btn-1").style.color = "white";
+  document.getElementById("btn-1").style.bordercolor = "white";
+
 }
 
   function iniciarSesion(){
@@ -30,7 +42,8 @@ event.preventDefault();
         document.getElementById("registro").style.display = "none"
         document.getElementById("inicioDeSesion").style.display = "none"
         document.getElementById("usuario").style.display = "block"
-        let mail = document.getElementById("emaili").value;
+        let mail = document.getElementById("emaili", "email").value;
+        usuarioActual = mail
         console.log(mail)
         const array = mail.split("@")
         console.log(array)
@@ -38,9 +51,10 @@ event.preventDefault();
         console.log(usuario)
         document.getElementById("usuario").innerText = usuario
         document.getElementById("borrarCuenta").style.display = "block"
+        $("#staticBackdrop").modal("hide");
       }
       else{
-
+          alert("El correo o la contraseña no son correctos")
       }
     });
   });
@@ -50,7 +64,10 @@ event.preventDefault();
         document.getElementById("registro").style.display = "block"
         document.getElementById("inicioDeSesion").style.display = "block"
         document.getElementById("usuario").style.display = "none"
+        document.getElementById("borrarCuenta").style.display = "none"
   }
   function eliminarCuenta(){
-    fetch("http://localhost:8080/api/borrarUsuario/" + document.getElementById("emaili").value)
+    fetch("http://localhost:8080/api/borrarUsuario/" + usuarioActual)
   }
+
+  
